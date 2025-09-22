3d09ea33eb32e6130b38be0168b6874ace90360c
newjeasnz <jesiccatandra@gmail.com>
2025-09-22 20:52:54 +0700

    Menghubungkan model Product dengan User, menampilkan last login user

--- main/views.py
+++ main/views.py
@@ -7,15 +7,24 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 @login_required(login_url='/login')
 def show_main(request):
-    products = Product.objects.all()
+    filter_type = request.GET.get("filter", "all")
+    if filter_type == "all":
+        products = Product.objects.all()
+    else:
+        products = Product.objects.filter(user=request.user)
+        
     context = {
         'app_name': 'THUNDR clubs',
-        'name': 'Jessica Tandra',
+        'name': request.user.username,
         'class': 'PBP B',
-        'product_list': products
+        'product_list': products,
+        'last_login': request.COOKIES.get('last_login', 'Never')
     }
 
     return render(request, "main.html", context)
@@ -24,10 +33,14 @@ def create_product(request):
     form = ProductForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        news_entry = form.save(commit = False)
+        news_entry.user = request.user
+        news_entry.save()
         return redirect('main:show_main')
     
-    context = {'form': form}
+    context = {
+        'form': form
+        }
     return render(request, "create_product.html", context)
 
 @login_required(login_url='/login')
@@ -80,7 +93,9 @@ def login_user(request):
       if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('main:show_main')
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
 
    else:
       form = AuthenticationForm(request)
@@ -89,4 +104,6 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
-    return redirect('main:login')
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
